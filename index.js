30c533b08323a1ae54d23814a2ee155451b0eead
AkChoudhary0 <choudhary22anil@gmail.com>
2025-03-01 15:35:26 +0530

    checking

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const http = require('http')
 var cors = require('cors')
-app.use(cors())
 var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
 
@@ -16,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
