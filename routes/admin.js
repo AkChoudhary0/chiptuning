6e539def53fa1a57cc3eb5f0a0c17135cc1ab808
Amit8566 <aynr135001@gmail.com>
2025-03-11 18:35:58 +0530

    hello message ak

--- routes/admin.js
+++ routes/admin.js
@@ -1,38 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const adminController = require('../controllers/admin/adminController.js')
-const loginController = require('../controllers/admin/loginController.js')
-const { verifyToken } = require("../config/auth.js")
+const adminController = require("../controllers/admin/adminController.js");
+const loginController = require("../controllers/admin/loginController.js");
+const { verifyToken } = require("../config/auth.js");
 
 /* GET home page. */
 // user api's routes
 
-router.post("/login", loginController.login)
-router.get("/createSuperAdmin", loginController.createSuperAdmin)
+router.post("/login", loginController.login);
+router.get("/createSuperAdmin", loginController.createSuperAdmin);
+router.get("/", (req,res) => {
+ res.send({
+    message:"hello"
+ })
+});
 
+router.post("/createMake", [verifyToken], adminController.createMake);
+router.post("/createGeneration", adminController.createGeneration);
+router.post("/createModel", adminController.createModel);
+router.post("/createEngine", adminController.createEngine);
+router.post("/getGeneration/:generationType", adminController.getGeneration);
+router.post("/getModels/:modelType", adminController.getModels);
+router.post("/listUsers", [verifyToken], adminController.getUserList);
+router.put(
+  "/updateUser/:userId",
+  [verifyToken],
+  adminController.updateUserDetail
+);
+router.post("/getEngine/:engineType", adminController.getEngine);
+router.post("/getEngineDetail", adminController.getEngineDetail);
+router.get("/getModelByMakeId/:makeId", adminController.getModelByMakeId);
+router.get("/getEngineById/:engineId", adminController.getEngineById);
+router.post("/getECU/", adminController.getECU);
 
-
-router.post("/createMake", [verifyToken], adminController.createMake)
-router.post("/createGeneration", adminController.createGeneration)
-router.post("/createModel", adminController.createModel)
-router.post("/createEngine", adminController.createEngine)
-router.post("/getGeneration/:generationType", adminController.getGeneration)
-router.post("/getModels/:modelType", adminController.getModels)
-router.post("/listUsers", [verifyToken], adminController.getUserList)
-router.put("/updateUser/:userId", [verifyToken], adminController.updateUserDetail)
-router.post("/getEngine/:engineType", adminController.getEngine)
-router.post("/getEngineDetail", adminController.getEngineDetail)
-router.get("/getModelByMakeId/:makeId", adminController.getModelByMakeId)
-router.get("/getEngineById/:engineId", adminController.getEngineById)
-router.post("/getECU/", adminController.getECU)
-
-router.post("/addECU", adminController.addECU)
-router.post("/getMakes/:makeType", adminController.getMakes)
-router.post("/getVehicleDropDown/:type", adminController.getVehicleDropDown)
-router.delete("/deleteMakeById/:makeId", adminController.deleteMakeById)
-router.delete("/deleteGenerationById/:generationId", adminController.deleteGenerationById)
-router.delete("/deleteEngineById/:engineId", adminController.deleteEngineById)
-router.delete("/deleteModelById/:modelId", adminController.deleteModelById)
+router.post("/addECU", adminController.addECU);
+router.post("/getMakes/:makeType", adminController.getMakes);
+router.post("/getVehicleDropDown/:type", adminController.getVehicleDropDown);
+router.delete("/deleteMakeById/:makeId", adminController.deleteMakeById);
+router.delete(
+  "/deleteGenerationById/:generationId",
+  adminController.deleteGenerationById
+);
+router.delete("/deleteEngineById/:engineId", adminController.deleteEngineById);
+router.delete("/deleteModelById/:modelId", adminController.deleteModelById);
 
 // router.post("/createMake",adminController.createMake)
 
