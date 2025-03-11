6e6b65d9112faf49007c73f4e65e7b0c366862b3
AkChoudhary0 <choudhary22anil@gmail.com>
2025-03-11 10:56:31 +0530

    login api 11 march

---
+++ models/user/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    email: {
+        type: String,
+        required: [true, "email is required"],
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+    },
+    firstName: {
+        type: String,
+        default: null,
+    },
+    lastName: {
+        type: String,
+        default: null,
+    },
+    phone: {
+        type: String,
+        default: null,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('users', user);  //export the model
