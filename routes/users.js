6460b27605af50609d8fa038fb6cfe649387f30f
Amit8566 <aynr135001@gmail.com>
2025-03-12 14:51:19 +0530

    changes to the beauti by amit

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var userController = require("../controllers/admin/userController")
-
+var loginController = require("../controllers/admin/loginController")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post("/login", loginController.login);
 
 router.post("/getVehicleDropDown/:type", userController.getVehicleDropDown);
 router.get("/getEngineById/:engineId", userController.getEngineById);
