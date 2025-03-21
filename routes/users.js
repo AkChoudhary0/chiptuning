7230701dd51307cf0c6d741aee85217deb248b0e
Amit8566 <aynr135001@gmail.com>
2025-03-21 12:24:54 +0530

    checking the data

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,7 @@ router.get("/", function (req, res, next) {
 router.post("/login", loginController.userLogin);
 router.post("/registerUser", loginController.registerUser);
 router.post("/getEngine/:engineType", adminController.getEngine);
+router.get("/getEngineById/:engineId", adminController.getEngineById);
 
 router.post("/getVehicleDropDown/:type", userController.getVehicleDropDown);
 router.post("/saveFileServiceForm", userController.saveFileServiceForm);
