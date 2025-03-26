3070d91fcd6a9dcfc42d0b0f4dc041396ef0de07
Amit8566 <aynr135001@gmail.com>
2025-03-26 12:04:35 +0530

    changes to the chiptuning bya mi

--- routes/admin.js
+++ routes/admin.js
@@ -35,7 +35,7 @@ router.post("/getECU/", adminController.getECU);
 
 router.post("/addECU", adminController.addECU);
 router.post("/getMakes/:makeType", adminController.getMakes);
-router.post("/getVehicleDropDown/type", adminController.getVehicleDropDown);
+router.post("/getVehicleDropDown/:type", adminController.getVehicleDropDown);
 router.get("/getDropDownForOri", adminController.getDropDownForOri);
 // router.post("/getVehicleDropDown/original_file", adminController.getVehicleDropDown1);
 router.delete("/deleteMakeById/:makeId", adminController.deleteMakeById);
